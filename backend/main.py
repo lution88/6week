b67039931f48626d64bb5166bc1edbe8a8bc6df3
JKYOON_88 <78908697+lution88@users.noreply.github.com>
2026-08-12 00:49:52 +0900

    백엔드 CURD done, UI연결 시작

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
 
+SUBJECTS_LIST = [{"id": 1, "name": "Work"}, {"id": 2, "name": "Reading"}]
+NEXT_ID = 3
+
 
 @app.route("/subjects", methods=["GET"])
 def get_subjects():
-    return jsonify([{"id": 1, "name": "Work"}, {"id": 2, "name": "Reading"}])
+    return jsonify(SUBJECTS_LIST)
+
+
+@app.route("/subjects", methods=["POST"])
+def add_subject():
+    global NEXT_ID
+    exercise = request.get_json()
+    data = {"id": NEXT_ID, "name": exercise["name"]}
+    SUBJECTS_LIST.append(data)
+    NEXT_ID = NEXT_ID + 1
+    return data
+
+
+@app.route("/subjects/<int:id>", methods=["DELETE"])
+def delete_subject(id):
+    global SUBJECTS_LIST
+    SUBJECTS_LIST = [sub for sub in SUBJECTS_LIST if sub["id"] != id]
+    return {"success": True}
 
 
 if __name__ == "__main__":
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const [name, setName] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [subjects, setSubjects] = useState([]);
     const getName = async () => {
         const res = await fetch("http://127.0.0.1:5001/subjects");
         const json = await res.json();
-        console.log(json);
+        setSubjects(json);
+        setLoading(false);
     };
     useEffect(() => {
         getName();
@@ -14,10 +16,17 @@ function App() {
 
     return (
         <div>
-            <ul>
-                <li></li>
-                <li></li>
-            </ul>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <ul>
+                    {subjects.map((subject) => (
+                        <li key={subject.id}>
+                            <h3>{subject.name}</h3>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
