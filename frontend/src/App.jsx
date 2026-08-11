72c2daabf50a403513de422a2fcd791bda18630e
JKYOON_88 <78908697+lution88@users.noreply.github.com>
2026-08-11 23:46:35 +0900

    frontend: fetch 테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const [name, setName] = useState();
-
+    const [name, setName] = useState([]);
+    const getName = async () => {
+        const res = await fetch("http://127.0.0.1:5001/subjects");
+        const json = await res.json();
+        console.log(json);
+    };
     useEffect(() => {
-        const res = fetch("http://127.0.0.1:5001/subjects").then((data) => {
-            res.json();
-        });
-        console.log(data);
+        getName();
     }, []);
 
     return (
